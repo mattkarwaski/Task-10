61a75933bf1ce89c8494060120b5c9ac1697cb6d
mattkarwaski <75523988+mattkarwaski@users.noreply.github.com>
2020-12-17 22:45:49 +0100

    Add files via upload

---
+++ task.js
@@ -0,0 +1,21 @@
+var num1 = 10;
+    num2 = 5;
+    num3 = 2;
+
+function add() {
+  return num1 + num2;
+}
+
+function subtract() {
+  return num1 - num2;
+}
+
+function divide() {
+  return num1 / num2;
+}
+
+function math() {
+  alert((num1 + num2) / num3);
+}
+
+math();
